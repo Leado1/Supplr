19af99b5260c0851fc3dc471cfea85e763b0118e
leado1 <McGhosr123@gmail.com>
2026-02-17 16:33:35 -0600

    home page redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { redirect } from "next/navigation";
-import { auth } from "@clerk/nextjs/server";
 import { HomepageContent } from "./homepage-content";
 
-export default async function HomePage() {
-  const { userId } = await auth();
-
-  // If user is authenticated, redirect to dashboard
-  if (userId) {
-    redirect("/dashboard");
-  }
-
+export default function HomePage() {
   return <HomepageContent />;
 }
