2b250e0f9cc337b42c99d9cbd33e231a645d9dc5
leado1 <McGhosr123@gmail.com>
2026-02-17 13:14:04 -0600

    hero page image updates

--- src/components/marketing/layered-dashboard.tsx
+++ src/components/marketing/layered-dashboard.tsx
@@ -3,6 +3,8 @@
 import * as React from "react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import dashboardImage from "../../../public/images/dashboard.png";
+import sidebarImage from "../../../public/images/sidebar.png";
 
 interface LayeredDashboardProps {
   className?: string;
@@ -10,36 +12,28 @@ interface LayeredDashboardProps {
 
 export function LayeredDashboard({ className }: LayeredDashboardProps) {
   return (
-    <div className={cn("relative mx-auto w-full aspect-[1803/894]", className)}>
-      <div className="relative h-full w-full">
-        <div className="rounded-[42px] bg-gradient-to-br from-[#F3D6C6] via-[#D7C6F6] to-[#4B3CF7] p-[2px] shadow-[0_35px_90px_-60px_rgba(76,58,120,0.55)]">
-          <div className="overflow-hidden rounded-[40px] bg-white">
-            <Image
-              src="/images/dashboard.png?v=5"
-              alt="Supplr dashboard preview"
-              width={1803}
-              height={894}
-              className="h-full w-full object-contain rounded-[40px]"
-              priority
-              unoptimized
-            />
-          </div>
-        </div>
+    <div className={cn("relative mx-auto w-full", className)}>
+      <div className="relative w-full">
+        <Image
+          src={dashboardImage}
+          alt="Supplr dashboard preview"
+          className="block h-auto w-full"
+          priority
+          unoptimized
+        />
 
         <Image
-          src="/images/sidebar.png"
+          src={sidebarImage}
           alt="Supplr dashboard sidebar"
-          width={420}
-          height={780}
           className="absolute left-[-5%] top-[6%] hidden w-[18%] rounded-[26px] shadow-[0_30px_60px_rgba(36,20,74,0.35)] md:block"
           unoptimized
         />
 
         <Image
-          src="/images/inventorydetails.png"
+          src="/images/inventorydetails.png?v=2"
           alt="Supplr inventory details"
-          width={420}
-          height={420}
+          width={555}
+          height={384}
           className="absolute right-[-6%] top-[62%] hidden w-[26%] rounded-[22px] shadow-[0_30px_60px_rgba(36,20,74,0.35)] md:block"
           unoptimized
         />
